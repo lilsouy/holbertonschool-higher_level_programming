426db62106974ea7b7f596b7c4093468fa224f7c
lilsouy <solaff.alessa@gmail.com>
2026-01-14 16:25:39 +0000

    new update

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[52:80])
+print(str)
