9c09c74aec1f82074de8c766789b41588831279b
lilsouy <solaff.alessa@gmail.com>
2026-01-15 13:17:49 +0000

    print last digit with positive, negative, and zero handling

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    x = abs(number)
+    last = x % 10
+    last = -last
+else: 
+    last = number % 10
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif last == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+elif last < 6 and last !=0:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
