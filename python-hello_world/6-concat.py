9b3842ee936957cdd601d9b65a3ed3d21f7f6f92
lilsouy <solaff.alessa@gmail.com>
2026-01-17 01:20:25 +0000

    update

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# Concatenate str1 and str2 to print the message
-print(f"Welcome to {str1} {str2}!")
+str1 = "Welcome to {} {}!".format(str1, str2)
+print(str1)
